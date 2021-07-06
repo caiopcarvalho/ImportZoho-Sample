170da577d9b923c6df960e14787ccc70a3adebd4
Wiki Consultoria <delanno@wikiconsultoria.com>
2021-07-06 09:02:17 -0300

    search record

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+require('dotenv').config();
 const ImportZoho = require('importzoho');
 
-console.log('ok');
+//Busca registros pelo ID
+const getRecordById = async ()=>{
+
+    console.log('ok');
+
+    try{
+        let id = '116652000058774195';
+        console.log(typeof id)
+        let importacaoTeste = await ImportZoho.getId("ImportacaoTeste",id);
+        console.log(importacaoTeste)
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+//
+const getRecords = async ()=>{
+    try{
+        let records = await ImportZoho.getRecords("ImportacaoTeste",{"params":{ page: 1, per_page: 200 }})
+        console.log(records)
+
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+const getRecordModifiedSince = async ()=>{
+    try{
+        let records = await ImportZoho.getRecords("ImportacaoTeste",{ "modified_since": "2020-02-03T00:00:00-03:00" })
+        console.log(records.size())
+
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+const searchRecord = async ()=>{
+    try{
+        let records = await ImportZoho.searchRecords("ImportacaoTeste","(Name:equals:$_name)",()=>{},[{name: "Teste%2333799_2021_02_24"}])
+        console.log(records[0].response)
+
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+//searchRecord()
+
+//getRecordModifiedSince()
+//getRecords
+//getRecordById()
